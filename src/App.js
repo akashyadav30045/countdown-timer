d5782d0e1d44bcadc8b32a39a629bccb878c29a3
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2024-03-19 21:04:31 +0530

    Emi-Calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import "./App.css";
+import {useState} from "react";
+import { useRef } from "react";
+
 
 function App() {
+  const[isclicked,setisClicked] = useState(true);
+  const[hours,setHours] = useState(0);
+  const[minutes,setMinutes] = useState(0);
+  const[seconds,setSeconds] = useState(0);
+  const[tid,setTid] = useState(0)
+
+const handleinput = (e) => {
+  const classe = e.target.className;
+  const iput = parseInt(e.target.value);
+
+  if(classe === 'Hour'){
+    setHours(iput);
+  }
+  if(classe === "Minute"){
+    setMinutes(iput);
+  }
+  if(classe === "Second"){
+    setSeconds(iput);
+  }
+}
+console.log(hours,minutes,seconds)
+
+const Decrease = (seconds, hours, minutes, isclicked) => {
+  if(seconds>0){
+    setSeconds(seconds-1)
+  }
+   if(minutes>0 && seconds===0){
+    setMinutes(minutes-1);
+    setSeconds(59)
+  }
+  if(hours>0 && minutes===0){
+    setHours(hours-1);
+    setMinutes(59)
+    setSeconds(59)
+  }
+}
+
+useEffect(() => {
+  if (!isclicked) {
+    const intervalId = setInterval(() => {
+      Decrease(seconds, hours, minutes, isclicked);
+    }, 1000);
+
+    setTid(intervalId);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }
+}, [seconds, hours, minutes, isclicked]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="full">
+      
+      <h1>Countdown Timer.</h1>
+      {isclicked && 
+      <div className="skeleton">
+        <input onChange={handleinput}
+         type="text" className="Hour" placeholder="HH" />
+      
+        <input type="text" onChange={handleinput} className="Minute" placeholder="MM" />
+        <input type="text" onChange={handleinput} className="Second" placeholder="SS" />
+        <button className="button" onClick={()=>setisClicked(!isclicked)}>Start</button>
+      </div>}
+      
+      {/* second one */}
+      {!isclicked &&
+      <div className="second3">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {hours}<span>:</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          {minutes}<span>:</span>
+        </p>
+        <p>{seconds}</p>
+        <button    className="pause">
+          Pause
+        </button>
+        <button onClick={()=>setisClicked(!isclicked)} className="reset">
+          Reset
+        </button>
+      </div>}
     </div>
+    
   );
 }
 
